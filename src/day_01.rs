8c8e215655697b7a7dad99361373fe27230b3665
Gabriel Cavalcante <gscavalcante@gmail.com>
2022-12-01 15:23:42 +0000

    Add second part of the day 01

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,8 +1,9 @@
-use std::fs;
+use std::{fs, ops::Add};
 
-pub fn calorie_counting(file_path: &str) -> i32 {
+pub fn calorie_counting_part_01(file_path: &str) -> i32 {
     let input = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+        .expect("Should have been able to read the file")
+        .add("\n ");
     let input_vec: Vec<&str> = input
         .lines()
         .collect();
@@ -11,7 +12,7 @@ pub fn calorie_counting(file_path: &str) -> i32 {
 
     let mut sum = 0;
     for calories in input_vec {
-        if calories.is_empty() {
+        if calories.trim().is_empty() {
             if sum > higher {
                 higher = sum;
             }
@@ -28,7 +29,49 @@ pub fn calorie_counting(file_path: &str) -> i32 {
 }
 
 #[test]
-fn part_01_with_example_data() {
-    let result = calorie_counting("inputs/day_01/example.txt");
+fn calorie_counting_part_01_with_example_data() {
+    let result = calorie_counting_part_01("inputs/day_01/example.txt");
     assert_eq!(result, 24000);
+}
+
+pub fn calorie_counting_part_02(file_path: &str) -> i32 {
+    let input = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file")
+        .add("\n ");
+    let input_vec: Vec<&str> = input
+        .lines()
+        .collect();
+
+    let mut top_three: [i32; 3] = [0; 3];
+
+    let mut sum = 0;
+    for calories in input_vec {
+        if calories.trim().is_empty() {
+            let mut higher = 0;
+            for index in 0..top_three.len() {
+                if sum > top_three[index] {
+                    higher += 1;
+                }
+            }
+
+            if higher > 0 {
+                top_three[0] = sum;
+                top_three.sort();
+            }
+
+            sum = 0;
+            continue;
+        }
+        
+        sum = sum + calories.parse::<i32>()
+            .expect("Should be a number");
+    }
+
+    top_three.iter().sum()
+}
+
+#[test]
+fn calorie_counting_part_02_with_example_data() {
+    let result = calorie_counting_part_02("inputs/day_01/example.txt");
+    assert_eq!(result, 45000);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day_01;
 
 fn main() {
-    day_01::calorie_counting("inputs/day_01/input.txt");
+    day_01::calorie_counting_part_01("inputs/day_01/input.txt");
+    day_01::calorie_counting_part_02("inputs/day_01/input.txt");
 }
